48dcf3612702fe9d97d4c565444690687c0c5777
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-10-06 11:56:08 +0200

    Add function convert_uchar_ptr_to_hex_string
    Add of function convert_uchar_ptr_to_hex_string.
    Converts uchar* ptr to hex string.
    uchar* ptr needed caused by MD5 c-style function.

--- src/file2passwd.cpp
+++ src/file2passwd.cpp
@@ -29,24 +29,30 @@ void file2passwd::get_file_buffer(std::string file_path, std::streamsize file_si
 	file.close();
 }
 
+std::string file2passwd::convert_uchar_ptr_to_hex_string(unsigned char* result)
+{
+	std::stringstream ss;
+	ss << std::hex << std::setfill('0');
+	for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
+	{
+	    ss << std::setw(2) << static_cast<unsigned>(result[i]);
+	}
+	return ss.str();
+}
+
 std::string file2passwd::get_md5_hash_from_file(std::string file_path)
 {
-	unsigned char result[MD5_DIGEST_LENGTH];
+	unsigned char* result = new unsigned char[MD5_DIGEST_LENGTH];
 
 	std::streamsize file_size = get_file_size(file_path);
 	char file_buffer[file_size];
 	get_file_buffer(file_path, file_size, file_buffer);
 
-	MD5((unsigned char*) file_buffer, file_size, (unsigned char*)result);
+	MD5((unsigned char*) file_buffer, file_size, result);
 	munmap((char*)file_buffer, file_size);
 
-	std::stringstream ss;
-	ss << std::hex << std::setfill('0');
-	for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
-	{
-	    ss << std::setw(2) << static_cast<unsigned>(gsl::at(result, i));
-	}
-	md5_from_file = ss.str();
+	md5_from_file = convert_uchar_ptr_to_hex_string(result);
 
+	delete result;
 	return md5_from_file;
 }
--- src/file2passwd.hpp
+++ src/file2passwd.hpp
@@ -9,6 +9,7 @@ class file2passwd {
 private:
 	std::streamsize get_file_size(std::string file_path);
 	void get_file_buffer(std::string file_path, std::streamsize file_size, char* file_buffer);
+	std::string convert_uchar_ptr_to_hex_string(unsigned char* result);
 
 public:
 	std::string md5_from_file;
