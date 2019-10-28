ffc01f0afe23be6784dca914112bd5c9b62c2291
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-10-28 08:52:45 +0100

    Add ignoreable lines
    Ignore specific lines in clang-tidy. Ignore c arrays and c-style casts, which are necessary for c-style function MD5 from OpenSSL::Crypto.

--- src/compatibility_layer.cpp
+++ src/compatibility_layer.cpp
@@ -7,6 +7,11 @@
 #include <openssl/md5.h>
 #include <gsl/gsl>
 
+/**
+ *  A class for compatibility to c.
+ *  c arrays and cstyle casts ignored in clang-tidy.
+ */
+
 compat_layer::compat_layer(std::string argv_file_path)
 {
 	file_path = argv_file_path;
@@ -38,24 +43,24 @@ std::string compat_layer::convert_uchar_ptr_to_hex_string(unsigned char* result)
 	ss << std::hex << std::setfill('0');
 	for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
 	{
-	    ss << std::setw(2) << static_cast<unsigned>(result[i]);
+	    ss << std::setw(2) << static_cast<unsigned>(result[i]); //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 	}
 	return ss.str();
 }
 
 std::string compat_layer::get_md5_hash_from_file(void)
 {
-	unsigned char* result = new unsigned char[MD5_DIGEST_LENGTH];
+	unsigned char* result = new unsigned char[MD5_DIGEST_LENGTH]; //NOLINT(cppcoreguidelines-owning-memory)
 
 	std::streamsize file_size = get_file_size();
-	char file_buffer[file_size];
-	get_file_buffer(file_size, file_buffer);
+	char file_buffer[file_size]; //NOLINT(cppcoreguidelines-avoid-c-arrays)
+	get_file_buffer(file_size, file_buffer); //NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 
-	MD5((unsigned char*) file_buffer, file_size, result);
+	MD5((unsigned char*) file_buffer, file_size, result); //NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
 	munmap((char*)file_buffer, file_size);
 
 	md5_from_file = convert_uchar_ptr_to_hex_string(result);
 
-	delete result;
+	delete result; //NOLINT(cppcoreguidelines-owning-memory)
 	return md5_from_file;
 }
