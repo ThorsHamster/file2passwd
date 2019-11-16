476de9c53eb336dc70740e52f62d978d0a7540d8
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-11-16 12:35:46 +0100

    Moved functions to private

--- src/file2passwd.hpp
+++ src/file2passwd.hpp
@@ -9,9 +9,7 @@
 class file2passwd {
 public:
 
-  std::string get_fibonacci_char_vector(void);
   std::string get_md5_hash_from_file(void);
-  std::string generate_passwd_from_file(int passwd_length);
   std::string get_passwd(void);
 
   file2passwd(std::string argv_file_path) : file_path(argv_file_path), compat(argv_file_path) {}
@@ -20,6 +18,9 @@ private:
   std::string file_path;
   compat_layer compat;
   std::string md5_from_file;
+
+  std::string get_fibonacci_char_vector(void);
+  std::string generate_passwd_from_file(int passwd_length);
 
   void handleErrors(void);
   int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
