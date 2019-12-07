aeb3cb890c192b67698bfc9a7ed7512baf7bb69c
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-12-07 18:13:54 +0100

    Deactivation of options
    Deactivated BUILD_DOC and GOOGLE_TESTS as standard.
    Add of Default parameters for travis build.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DCODE_COVERAGE=ON -DBUILD_DOC=OFF ..
+cmake -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . -j $(nproc)
 # Coverage
