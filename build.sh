853914211fae2b03bb4609e37053196a06d4800a
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-12-26 15:03:55 +0100

    Add of " in build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ mkdir -p build && cd build
 # Configure
 cmake -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
-cmake --build . -j $(nproc)
+cmake --build . -j "$(nproc)"
