d8c3c92a0a66e3f66947d6ff70b5cb354e469ab6
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-12-29 13:13:21 +0100

    Add doxygen docu to github pages

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON ..
+cmake -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON -DBUILD_DOC=ON ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . -j "$(nproc)"
