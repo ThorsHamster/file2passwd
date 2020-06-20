c795bfcfd5ec15324b30246dd0392d8f76a15dff
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-06-20 13:38:11 +0200

    Use git ls-file -m instead

--- check_styleguide.sh
+++ check_styleguide.sh
@@ -4,16 +4,18 @@ set -euo pipefail
 
 find . \( -name '*.hpp' -o -name '*.cpp' \) -exec clang-format -i {} +
 
-not_correct_list=`git status | grep "modified"`
+not_correct_list=`git ls-files -m`
 
 if [[ -z $not_correct_list ]]; then
-  echo "No styleguide violation with clang-format found."
+  echo "No styleguide violation with clang-format found.";
   exit 0;
 else
-  echo "Please check for following files:";
-  for f in $notcorrectlist; do
-      echo $f
-      git diff $f
+  echo "Please check following files:";
+  for f in $not_correct_list; do
+    echo $f
+    git diff $f
   done
   exit 1;
 fi
+
+git reset HEAD --hard
