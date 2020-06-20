5f293319afe0c8e6dd3866f44bb9f907d34165b5
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-06-20 14:37:52 +0200

    Removal of Codacy issue

--- check_styleguide.sh
+++ check_styleguide.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 find . \( -name '*.hpp' -o -name '*.cpp' \) -exec clang-format -i {} +
 
-not_correct_list=`git ls-files -m`
+not_correct_list=$(git ls-files -m)
 
 if [[ -z $not_correct_list ]]; then
   echo "No styleguide violation with clang-format found.";
