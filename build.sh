2dc4320d59a11dddf9b3513f91a5f12c52c7d104
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-09-20 11:34:57 +0200

    Add install target and copy python binaries to it

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON -DBUILD_DOC=ON -DBUILD_PYTHON=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=../_install -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON -DBUILD_DOC=ON -DBUILD_PYTHON=ON ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
-cmake --build . -j "$(nproc)"
+cmake --build . -j "$(nproc)" --target install
