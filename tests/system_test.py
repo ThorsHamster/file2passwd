f50292b678c69c1824f5e5ccbff7b22f1c7c21fb
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-09-20 11:48:00 +0200

    Add python test

---
+++ tests/system_test.py
@@ -0,0 +1,13 @@
+import unittest
+import pyfile2passwd
+
+
+class File2PasswdTests(unittest.TestCase):
+
+    def test_get_passwd(self):
+        fpo = pyfile2passwd.File2Passwd("LICENSE")
+        self.assertEqual(fpo.get_passwd(), "1eeb22037fbc98193fbd60f6cad7ec6c")
+
+
+if __name__ == '__main__':
+    unittest.main()
