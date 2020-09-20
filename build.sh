ec32f89a0d4da84cc4e74d623cc2373a8c10fbdd
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-09-20 11:49:55 +0200

    Add run of python test to build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=../_install -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON -DBUILD_DOC=ON -DBUILD_PYTHON=ON ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . -j "$(nproc)" --target install
+
+# run python tests
+cd _install/bin || exit
+python3 system_test.py
