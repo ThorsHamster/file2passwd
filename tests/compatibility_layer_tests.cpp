161b2f02029c2b58aa443ce17aa8004bdbcb4ba7
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-03 10:42:57 +0200

    Refactoring to OOP

--- tests/compatibility_layer_tests.cpp
+++ tests/compatibility_layer_tests.cpp
@@ -1,4 +1,5 @@
 
+#include <gmock/gmock.h>
 #include <limits.h>
 #include <string.h>
 
@@ -6,22 +7,38 @@
 #include "gtest/gtest.h"
 
 namespace {
-TEST(TEST_get_md5_hash_from_file, Trivial) {
-  CompatibilityLayer compat("LICENSE");
+
+class CompatibilityLayerTests : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+
+  }
+
+  virtual void ConfigureUnitUnderTest() {
+    unit_under_test_ = std::make_unique<CompatibilityLayer>("LICENSE");
+    //unit_under_test_->inject_test_seam(std::move(mock_compat_));
+  }
+
+  std::unique_ptr<CompatibilityLayer> unit_under_test_;
+};
+
+TEST_F(CompatibilityLayerTests, get_md5_hash_from_file_trivial) { 
+  ConfigureUnitUnderTest();
+
   const std::string expected_string = "5cbe034c343ead03a139a598a5d27d55";
-  const std::string result = compat.get_md5_hash_from_file();
+  const std::string result = unit_under_test_->get_md5_hash_from_file();
   EXPECT_EQ(result, expected_string);
 }
 
-TEST(TEST_convert_uchar_ptr_to_hex_string, Trivial) {
-  CompatibilityLayer compat("LICENSE");
+TEST_F(CompatibilityLayerTests, convert_uchar_ptr_to_hex_string_rivial) {
+  ConfigureUnitUnderTest();
   // see here for ascii table: https://www.torsten-horn.de/techdocs/ascii.htm
   // 1 equals 31.. a equals 61
   const std::string expected_string = "30313233616263643031323361626364";
 
   std::string test_string = "0123abcd0123abcd";
   auto test_vector = std::vector<unsigned char>(test_string.data(), test_string.data() + test_string.length());
-  const std::string result = compat.convert_uchar_ptr_to_hex_string(test_vector.data());
+  const std::string result = unit_under_test_->convert_uchar_ptr_to_hex_string(test_vector.data());
   EXPECT_EQ(result, expected_string);
 }
 }  // namespace
