615dfbcc76b4448d53c71f596aa285c26433f43b
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-04 17:33:32 +0200

    Add of test for get_passwd_complete

--- tests/file2passwd_tests.cpp
+++ tests/file2passwd_tests.cpp
@@ -72,6 +72,32 @@ TEST_F(File2PasswdTests, get_passwd_file_does_not_exist) {
   EXPECT_THROW(unit_under_test_->get_passwd(), FileDoesNotExistException);
 }
 
+TEST_F(File2PasswdTests, get_passwd_complete) {
+  ON_CALL(*mock_file_reader_, file_exists())
+      .WillByDefault(Return(true));
+  ON_CALL(*mock_compat_, get_md5_hash_from_file())
+      .WillByDefault(Return("abcdefghabcdefghabcdefghabcdefgh"));
+  std::vector<char> file_buffer = {'a', 'b', 'c', 'd', 'e'};
+  ON_CALL(*mock_file_reader_, get_file_buffer())
+      .WillByDefault(Return(file_buffer));
+  ON_CALL(*mock_utilities_, get_max_fibonacci_value())
+      .WillByDefault(Return(3));
+  EXPECT_CALL(*mock_utilities_, fibonacci(_))
+      .WillOnce(Return(1))
+      .WillOnce(Return(2))
+      .WillOnce(Return(3))
+      .WillRepeatedly(Return(100));
+  ON_CALL(*mock_compat_, encrypt(_, _, _))
+      .WillByDefault(Return("password"));
+
+  ConfigureUnitUnderTest();
+
+  auto result = unit_under_test_->get_passwd();
+  const std::string expected_string = "password";
+
+  EXPECT_EQ(result, expected_string);
+}
+
 }  // namespace
 
 int main(int argc, char **argv) {
