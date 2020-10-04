e69b8aa11b6cc6b7f94fb8875490b18bc1b0065c
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-04 16:48:47 +0200

    Use file_buffer.size()

--- src/compatibility_layer.cpp
+++ src/compatibility_layer.cpp
@@ -26,7 +26,7 @@ auto CompatibilityLayer::get_md5_hash_from_file(void) -> std::string {
   std::vector<char> file_buffer(file_size);                //NOLINT(cppcoreguidelines-avoid-c-arrays)
   file_buffer = file_reader_->get_file_buffer(file_size);  //NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 
-  MD5((unsigned char *)file_buffer.data(), file_size, result.get());  //NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
+  MD5((unsigned char *)file_buffer.data(), file_buffer.size(), result.get());  //NOLINT(cppcoreguidelines-pro-type-cstyle-cast)
 
   md5_from_file = convert_uchar_ptr_to_hex_string(result.get());
 
