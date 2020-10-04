21ec2b3320adf7cb1816c9d7bd82e7c4269f2996
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-04 16:34:21 +0200

    Test also for md5

--- tests/system_test.py
+++ tests/system_test.py
@@ -8,6 +8,10 @@ class File2PasswdTests(unittest.TestCase):
         fpo = pyfile2passwd.File2Passwd("LICENSE")
         self.assertEqual(fpo.get_passwd(), "1eeb22037fbc98193fbd60f6cad7ec6c")
 
+    def test_get_md5(self):
+        fpo = pyfile2passwd.File2Passwd("LICENSE")
+        self.assertEqual(fpo.get_md5_hash(), "5cbe034c343ead03a139a598a5d27d55")
+
 
 if __name__ == '__main__':
     unittest.main()
