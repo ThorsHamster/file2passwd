8532882c8e5cec51b6146f950a7a040d044a54b8
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-18 11:24:18 +0200

    Removal of unnecessary variable

--- src/openssl.cpp
+++ src/openssl.cpp
@@ -26,8 +26,6 @@ auto OpenSSL::encrypt(unsigned char *plaintext, int plaintext_len, unsigned char
 
   int len;
 
-  int ciphertext_len;
-
   /* Create and initialise the context */
   if (!(ctx = EVP_CIPHER_CTX_new()))
     handleErrors();
@@ -48,7 +46,6 @@ auto OpenSSL::encrypt(unsigned char *plaintext, int plaintext_len, unsigned char
    */
   if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
     handleErrors();
-  ciphertext_len = len;
 
   /*
    * Finalise the encryption. Further ciphertext bytes may be written at
@@ -56,7 +53,6 @@ auto OpenSSL::encrypt(unsigned char *plaintext, int plaintext_len, unsigned char
    */
   if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
     handleErrors();
-  ciphertext_len += len;
 
   /* Clean up */
   EVP_CIPHER_CTX_free(ctx);
