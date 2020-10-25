05daa8d88323229b4f9d1723fabeb8d99ef77906
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-10-25 11:40:35 +0100

    Using explicit keyword
    Codacy issue

--- tests/mock_libraries/mock_file_reader.hpp
+++ tests/mock_libraries/mock_file_reader.hpp
@@ -9,7 +9,7 @@ namespace filereader {
 
 class MockFileReader : public FileReaderInterface {
  public:
-  MockFileReader(const std::string &file_path) : FileReaderInterface("") {}
+  explicit MockFileReader(const std::string &file_path) : FileReaderInterface("") {}
   MOCK_METHOD(bool, file_exists, (), (override));
   MOCK_METHOD(std::vector<char>, get_file_buffer, (), (override));
   MOCK_METHOD(std::int64_t, get_file_size, (), (override));
