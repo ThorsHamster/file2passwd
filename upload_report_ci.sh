523e215a5981e89d6061c4a435698d1bacaf0616
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-11-24 17:48:17 +0100

    Add of own report ci upload file

--- upload_report_ci.sh
+++ upload_report_ci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Upload to Report.CI
-curl -s https://report.ci/upload.py | python - --include='*.xml' --framework=gtest --sha $GITHUB_SHA
+python report_ci_upload.py --include='*.xml' --framework=gtest
